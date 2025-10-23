46ae218a9f191960d414edcced0c7e958286fe56
miiduoa <demohan513@gmal.com>
2025-10-23 20:00:10 +0800

    Initial Commit

---
+++ tired/tiredApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiredApp.swift
+//  tired
+//
+//  Created by Han demo on 2025/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tiredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
